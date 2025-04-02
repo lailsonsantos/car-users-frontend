0a9b19042f0d743695b062be97741860f4b7e8dd
Lailson Santos <lailson2156@gmail.com>
2025-04-01 21:58:53 -0300

    Adicionadas as rotas publicas para acesso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'api/signin', pathMatch: 'full' },
+  { path: 'api/signin', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'api/users', loadComponent: () => import('./user/users/users.component').then(m => m.UsersComponent) },
+  { path: 'api/users/register', loadComponent: () => import('./user/user-registration/user-registration.component').then(m => m.UserRegistrationComponent) },
+  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  { path: 'cars', loadComponent: () => import('./car/cars/cars.component').then(m => m.CarsComponent) }
+];

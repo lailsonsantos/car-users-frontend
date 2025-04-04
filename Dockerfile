409767fe54f2e64c9279450ffb88a14672c5738b
Lailson Santos <lailson2156@gmail.com>
2025-04-04 20:41:52 -0300

    Preparando para implantação no Heroku

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 # Etapa 1: Build do Angular
-FROM node:22-alpine AS build
+FROM node:18 AS build
 
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-RUN npm run build --prod
+RUN npm run build -- --configuration production
 
-# Etapa 2: Servindo com Nginx
+# Etapa 2: Servir com Nginx
 FROM nginx:alpine
-COPY --from=build /app/dist/car-users-frontend /usr/share/nginx/html
+
 COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=build /app/dist/car-users-frontend/browser /usr/share/nginx/html
+
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.static(__dirname + '/dist/car-users-frontend'));
-
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname + '/dist/car-users-frontend/browser/index.html'));
-// });
-
-// const PORT = process.env.PORT || 4200;
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
-const distFolder = path.join(__dirname, 'dist', 'car-users-frontend');
-app.use(express.static(distFolder));
+app.use(express.static(__dirname + '/dist/car-users-frontend'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(distFolder, 'index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/car-users-frontend/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 4200;

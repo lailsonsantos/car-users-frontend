a76218bf210d64739d7a458dbc6fc1370475e224
Lailson Santos <lailson2156@gmail.com>
2025-04-04 23:43:06 -0300

    Atualizado o server.js

--- server.js
+++ server.js
@@ -13,20 +13,11 @@
 //   console.log(`Servidor rodando na porta ${PORT}`);
 // });
 
-// importar express
 const express = require('express');
-// iniciar express
+const path = require('path');
 const app = express();
-// nome da pasta no dist que sera feito o build
-const appName = 'car-users-frontend';
-// local onde build ira gerar os arquivos
-const outputPath = `${__dirname}/dist/${appName}`;
-
-// seta o diretorio de build para servir o conteudo Angular
-app.use(express.static(outputPath));
-// redirecionar qualquer requisicao para o index.html
-app.get('/*', (req, res) => {
-  res.sendFile(`${outputPath}/index.html`);
-});
-// ouvir a porta que o Heroku disponibilizar
-app.listen(process.env.PORT);
+app.use(express.static(__dirname + '/dist/car-users-frontend'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/car-users-frontend/browser/index.html'));});
+app.listen(process.env.PORT || 8080);

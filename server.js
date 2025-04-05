f5d139a8b1ee52427bb2dd1949354f01164e07d8
Lailson Santos <lailson2156@gmail.com>
2025-04-04 23:57:21 -0300

    Atualizado o server.js

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.static(__dirname + '/dist/car-users-frontend'));
-
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname + '/dist/car-users-frontend/browser/index.html'));
-// });
-
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(__dirname + '/dist/car-users-frontend'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/car-users-frontend/browser/index.html'));});
-app.listen(process.env.PORT || 8080);
+  res.sendFile(path.join(__dirname + '/dist/car-users-frontend/browser/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
